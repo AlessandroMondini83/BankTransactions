13a63c00550862b598a89ee7a1f2a91cc591a161
AlessandroMondini83 <alessandro.mondini@hotmail.it>
2025-07-11 15:10:39 +0200

    Update streamlit_app.py
    333

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import pandas as pd
 from datetime import datetime
 
 st.set_page_config(page_title="Elabora File Excel Multipli", layout="centered")
-st.title("📂 222 Preparazione file per RentGer")
+st.title("📂 333 Preparazione file per RentGer")
 
 uploaded_files = st.file_uploader(
     "Carica uno o più file Excel", type=["xlsx"], accept_multiple_files=True
